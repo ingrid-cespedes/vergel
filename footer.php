b7714a51a363151f3a67995605bf721f7de40a0e
Joaquin Martel <joaquinmartel@gmail.com>
2014-04-28 14:18:12 -0400

    sandbox theme

---
+++ footer.php
@@ -0,0 +1,12 @@
+	<div id="footer">
+		<span id="generator-link"><a href="http://wordpress.org/" title="<?php _e( 'WordPress', '' ) ?>" rel="generator"><?php _e( 'WordPress', 'sandbox' ) ?></a></span>
+		<span class="meta-sep">|</span>
+		<span id="theme-link"><a href="http://www.joaquinmartel.cl/" title="<?php _e( 'Martel Blank, theme for WordPress', '' ) ?>" rel="designer"><?php _e( 'Martel Blank', 'sandbox' ) ?></a></span>
+	</div><!-- #footer -->
+
+</div><!-- #wrapper .hfeed -->
+
+<?php wp_footer() ?>
+
+</body>
+</html>
