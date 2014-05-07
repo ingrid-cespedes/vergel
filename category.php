aa52d77b543cd455107513c60ea404384b81bfc1
Franco <francobaldassarerodriguez@gmail.com>
2014-05-07 19:41:43 -0400

    eliminado paginator y y edit margin paginavi

--- category.php
+++ category.php
@@ -49,15 +49,7 @@
     <!--Inicio de codigo de pagination-->
 	    <?php wp_pagenavi(); //Posts por páginas ?>
     
-        <ul class="pagination topp">
-            <li class="disabled"><a href="#">&laquo;</a></li>
-            <li class="active"><a href="#">1</a></li>
-            <li><a href="#">2</a></li>
-            <li><a href="#">3</a></li>
-            <li><a href="#">4</a></li>
-            <li><a href="#">5</a></li>
-            <li><a href="#">&raquo;</a></li>
-        </ul>
+        
 	
 	<!-- </div> ¿.row? -->
   </div><!--cierre de container-->
