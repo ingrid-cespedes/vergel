7b69953a4675c219a8109122a168ba1374f465cb
ingrid-cespedes <ingrid.cespedes.alfaro1@gmail.com>
2014-05-08 15:27:31 -0400

    cambio en category.php prueba

--- category.php
+++ category.php
@@ -26,7 +26,7 @@
 
       <!--  <div class="row"> -->
 		<?php
-		  $paged = get_query_var('paged') ? get_query_var('paged') : 4;
+		  $paged = get_query_var('paged') ? get_query_var('paged') : 1;
 		  $wp_query = new WP_Query(array('cat' => $current_cat_id, 'paged' => $paged));
 		  while ($wp_query->have_posts()) : $wp_query->the_post();
 		?>
@@ -44,6 +44,32 @@
 		                </a>
 					</div>
 
+					<div class="col-md-3">
+						<a href="<?php the_permalink() ?>">
+			                <div class="thumbnail">
+			                	<?php the_post_thumbnail('medium');?>
+			                    <div class="caption">
+			                    	<h4><?php the_title(); ?></h4>
+									<h6><?php the_time ('l j F, Y'); ?></h6>
+			                        <h5><?php echo string_limit_words(get_the_excerpt(), 10); echo '...' ?></h5>
+			                    </div>
+							</div>
+		                </a>
+					</div>
+
+					<div class="col-md-3">
+						<a href="<?php the_permalink() ?>">
+			                <div class="thumbnail">
+			                	<?php the_post_thumbnail('medium');?>
+			                    <div class="caption">
+			                    	<h4><?php the_title(); ?></h4>
+									<h6><?php the_time ('l j F, Y'); ?></h6>
+			                        <h5><?php echo string_limit_words(get_the_excerpt(), 10); echo '...' ?></h5>
+			                    </div>
+							</div>
+		                </a>
+					</div>
+					
 	<?php endwhile; wp_reset_postdata(); ?>
 
     <!--Inicio de codigo de pagination-->
