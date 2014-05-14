5077ed8315ffaf28d8c73f5a9e3e8aa4dfa3f7f2
Herbert Spencer <herbert.spencer@gmail.com>
2014-05-14 07:51:11 -0400

    header with display_name instead of user_login

--- header.php
+++ header.php
@@ -58,7 +58,7 @@
 			
 				<ul class="nav navbar-nav navbar-right">
 					<li class="dropdown">
-						<a href="#" class="dropdown-toggle" data-toggle="dropdown"><?php echo $current_user->user_login; ?><b class="caret"></b></a>
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown"><?php echo $current_user->display_name; ?><b class="caret"></b></a>
 							<ul class="dropdown-menu">
 								<?php echo '<li><a href="'.esc_url( get_permalink( get_page_by_title( __("Editar perfil", "vergel")))).'">Editar perfil</a></li>';	?>				
 								<?php echo '<li><a href="'.wp_logout_url( home_url() ).'">Salir</a></li>'; ?>
