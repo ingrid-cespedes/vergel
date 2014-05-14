7c0efdae10355e6162948def6e8705420bb8c5e2
Franco <francobaldassarerodriguez@gmail.com>
2014-05-14 13:51:19 -0400

    mod col 2 de navbar para footer

--- footer.php
+++ footer.php
@@ -14,6 +14,16 @@
             );
         ?>
 	</div>
+     <div class="navbar-header col-md-2">
+            <span class='cabecera visible-xs'><?php bloginfo('name'); ?></span>
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#collapsible">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="<?php echo site_url(); ?>"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/logo-vergel.png"></img></a>
+        </div>
 </footer>
 
 <?php wp_footer() ?>
