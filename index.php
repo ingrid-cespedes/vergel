2effbef93f3df336b7e9f55ca3b6965c7774aa39
ingrid-cespedes <ingrid.cespedes.alfaro1@gmail.com>
2014-05-14 16:42:48 -0400

    padding al caption

--- index.php
+++ index.php
@@ -5,8 +5,8 @@
 <div class="jumbotron margin-top-fix">
     <div class="container">
 
-        <h1>Concurso de ideas y relatos</h1>
-        <p>Ideas para convertir las ruinas del Incendio de Valparaíso en un espacio socio-cultural</p>
+        <h1>Concurso de relatos e ideas</h1>
+        <p>Lee las <a href="<?php echo esc_url( get_permalink( get_page_by_title( 'Concurso' ) ) ); ?>">bases del concurso</a> y comparte tu experiencia</p>
 
 		<?php global $current_user; get_currentuserinfo(); if (is_user_logged_in () ) { ?>
     		<p>Ya estás registrado, ahora sólo te falta compartir tu historia.</p>
