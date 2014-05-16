bdc41e531edc130321bc78538812d42de92cda5c
Franco <francobaldassarerodriguez@gmail.com>
2014-05-16 12:57:17 -0400

    Agregado script de google analytics en header.php

--- header.php
+++ header.php
@@ -18,8 +18,18 @@
 
 </head>
 
-<body class="<?php sandbox_body_class() ?>">
 
+<body class="<?php sandbox_body_class() ?>">
+	<script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+	  ga('create', 'UA-51061031-1', 'vergel439.cl');
+	  ga('send', 'pageview');
+
+	</script>
 	
 	<header>
 	  <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
