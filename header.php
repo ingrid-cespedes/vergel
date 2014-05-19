81a7d3b553c58b21cd545ef6b13d7a6b74f19438
Franco <francobaldassarerodriguez@gmail.com>
2014-05-19 15:59:22 -0400

    Add script type en head

--- header.php
+++ header.php
@@ -15,7 +15,7 @@
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
     <script src="<?php echo get_stylesheet_directory_uri(); ?>/js/bootstrap.min.js"></script>
-
+    <script type="text/javascript" src="/nhpup_1.1.js"></script>
 </head>
 
 <body class="<?php sandbox_body_class() ?>">
