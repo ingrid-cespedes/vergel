2ec1ece45dc328b0adab392e2e48e70bd3b288f0
Franco <francobaldassarerodriguez@gmail.com>
2014-05-19 16:41:39 -0400

    agregado script Jquery

--- header.php
+++ header.php
@@ -15,6 +15,7 @@
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
     <script src="<?php echo get_stylesheet_directory_uri(); ?>/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="jquery-1.9.1.min.js"></script>
     <script type="text/javascript" src="/nhpup_1.1.js"></script>
 </head>
 
