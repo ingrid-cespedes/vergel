96ea321f33cf7824b1328178d26b520ab32ee0b3
Franco <francobaldassarerodriguez@gmail.com>
2014-05-23 19:56:04 -0400

    Cambio en en category, 30

--- index.php
+++ index.php
@@ -33,7 +33,8 @@
 			                <div class="thumbnail-relatos">
 			                	<a href="<?php the_permalink() ?>">
 			                    <div class="caption">
-			                    	<h4><?php the_title(); ?></h4>
+			                    	<h5><?php the_title(); ?></h5>
+			                    	<aside><?php the_time ('l j F, Y'); ?></aside>
 			                        <p><?php echo string_limit_words(get_the_excerpt(), 45); echo '...' ?></p>
 			                    </div>
 							</div>
@@ -51,7 +52,6 @@
 
 			                <div class="thumbnail-relatos">
 			                	<a href="<?php the_permalink() ?>">
-			                	<?php the_post_thumbnail('small');?>
 			                    <div class="caption">
 			                    	<h5><?php the_title(); ?></h5>
 			                    	<aside><?php the_time ('l j F, Y'); ?></aside>
