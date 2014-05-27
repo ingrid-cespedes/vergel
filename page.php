4ef19d0e2ce459d545aa0ac8d1db55badfb0b486
Franco <francobaldassarerodriguez@gmail.com>
2014-05-27 14:39:41 -0400

    page author

--- page.php
+++ page.php
@@ -5,26 +5,26 @@ Template Name: Página base
 ?>
 
 <?php get_header() ?>
+	<div class="container900">
+	    <div class="container margin-top-fix">
+			<?php the_post() ?>
+			
+			<div class="row">
+	        	<div class="col-md-12">
+					<div class="thumbnail">
+	                	<div class="captiondos">		
+							<h1><?php the_title() ?></h1>
+							<?php the_content() ?>
 
-    <div class="container margin-top-fix">
-		<?php the_post() ?>
-		
-		<div class="row">
-        	<div class="col-md-12">
-				<div class="thumbnail">
-                	<div class="captiondos">		
-						<h1><?php the_title() ?></h1>
-						<?php the_content() ?>
 
 
+						</div>
 
-					</div>
-
+						</div>
 					</div>
 				</div>
-			</div>
-			
-		</div><!-- .row -->
-    </div> <!-- container -->
-
+				
+			</div><!-- .row -->
+	    </div> <!-- container -->
+	</div>
 <?php get_footer() ?>
