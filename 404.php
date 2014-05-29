dd87138e095126604a9a9ead41a64ab6e6616b4e
Franco <francobaldassarerodriguez@gmail.com>
2014-05-29 14:39:27 -0400

    Page Error
    Page error Creada

--- 404.php
+++ 404.php
@@ -1,23 +1,19 @@
 <?php get_header() ?>
 
-	<div id="container">
-		<div id="content">
+	<div class="background-croquis">
+	<div class="container800">
+	    <div class="container margin-top-fix">
 
-			<div id="post-0" class="post error404 not-found">
-				<h2 class="entry-title"><?php _e( 'Not Found', 'sandbox' ) ?></h2>
-				<div class="entry-content">
-					<p><?php _e( 'Apologies, but we were unable to find what you were looking for. Perhaps  searching will help.', 'sandbox' ) ?></p>
-				</div>
-				<form id="searchform-404" class="blog-search" method="get" action="<?php bloginfo('home') ?>">
-					<div>
-						<input id="s-404" name="s" class="text" type="text" value="<?php the_search_query() ?>" size="40" />
-						<input class="button" type="submit" value="<?php _e( 'Find', 'sandbox' ) ?>" />
+				<div id="post-0" class="post error404 not-found">
+					<div class="encabezado"><h2><?php _e( 'Error 404', 'sandbox' ) ?></h2></div>
+					<div class="entry-content">
+						<h4><?php _e( 'La página que estás buscando, no ha sido encontrada.', 'sandbox' ) ?></h4>
 					</div>
-				</form>
-			</div><!-- .post -->
+					<form id="searchform-404" class="blog-search" method="get" action="<?php bloginfo('home') ?>">
+					</form>
+				</div><!-- .post -->
 
-		</div><!-- #content -->
-	</div><!-- #container -->
-
-<?php get_sidebar() ?>
+			</div><!-- #container margin top fix-->
+		</div><!-- background croquis -->
+</div>
 <?php get_footer() ?>
